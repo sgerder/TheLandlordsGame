b52431aa39fbbe30713075712cffc547f1281242
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-09-18 09:28:55 +0200

    Tilføjet nogle værdier til playerClass

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Player {
+    String name;
+    ArrayList<> portfolio = new ArrayList<>();
+    int wealth;
+    int turnOrderPosition;
+
+    Player (String playerName, int position) {
+        this.name = playerName;
+        wealth = 30000;
+        this.turnOrderPosition = position;
+    }
+
 }
